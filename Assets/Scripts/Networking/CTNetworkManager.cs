fc96f3f1fbf13203ae8880ad0de7ae75855c96b2
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-05-13 18:03:38 -0300

    NEW: Unit movement

---
+++ Assets/Scripts/Networking/CTNetworkManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace CraftTanks.Networking 
+{
+    public class CTNetworkManager : NetworkManager
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.AI;
+
+namespace CraftTanks.Units 
+{
+    public class UnitMovement : NetworkBehaviour
+    {
+        [Header("CORE")]
+        private NavMeshAgent agent = null;
+        private Camera mainCamera;
+
+        #region Server
+
+        [Command]
+        private void CmdMove(Vector3 position)
+        {
+            if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas))
+            {
+                return;
+            }
+
+            agent.SetDestination(hit.position);
+        }
+
+        #endregion
+
+        #region Client
+
+        public override void OnStartAuthority()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            mainCamera = Camera.main;
+        }
+
+        [ClientCallback]
+        private void Update()
+        {
+            if (!hasAuthority || !Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                return;
+            }
+
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                CmdMove(hit.point);
+            }
+        }
+
+        #endregion
+    }
+}
