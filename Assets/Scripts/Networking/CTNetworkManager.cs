d6b189e7b7cbc71925cd58b5a3484c16d8633ca8
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-05-13 18:54:47 -0300

    NEW: unit spawner

--- Assets/Scripts/Networking/CTNetworkManager.cs
+++ Assets/Scripts/Networking/CTNetworkManager.cs
@@ -7,6 +7,15 @@ namespace CraftTanks.Networking
 {
     public class CTNetworkManager : NetworkManager
     {
-        
+        [Header("Units")]
+        [SerializeField] private GameObject unitSpawner = null;
+
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            base.OnServerAddPlayer(conn);
+
+            GameObject unitSpawnerInstance = Instantiate(unitSpawner, conn.identity.transform.position, conn.identity.transform.rotation);
+            NetworkServer.Spawn(unitSpawnerInstance, conn);
+        }
     }
 }
